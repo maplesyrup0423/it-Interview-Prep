3cf7681cdf91662bf87d6d1b0e44aaaa5d72bf33
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2025-01-04 01:49:37 +0900

    로그아웃시 홈으로 이동

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebaseConfig"; // Firebase 설정 파일에서 auth 가져오기
+import { useNavigate } from "react-router-dom";
 
 const Logout = ({ setUser }) => {
+  const navigate = useNavigate();
+
   const handleLogout = () => {
     signOut(auth)
       .then(() => {
         setUser(null); // 로그아웃 후 사용자 정보 초기화
+        navigate("/");
       })
       .catch((error) => {
         console.error("로그아웃 실패: ", error);
